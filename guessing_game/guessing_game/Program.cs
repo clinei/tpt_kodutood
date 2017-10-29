1ae2e51c0c98ff75c0ca5ff25e347da6726ecd01
clinei <tanel58@hotmail.com>
2017-10-29 17:47:00 +0200

    first task

---
+++ guessing_game/guessing_game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessing_game {
+    class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. \nProovi see ära arvata" );
+
+            uint target = 54;
+
+            Console.Write( "Sinu pakkumine: " );
+            uint guess = uint.Parse( Console.ReadLine() );
+
+            if ( guess > target ) {
+                Console.WriteLine( "Minu number on väiksem" );
+            }
+            else if ( guess < target ) {
+                Console.WriteLine( "Minu number on suurem" );
+            }
+        }
+    }
+}
